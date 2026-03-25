8205b2b9da24c6edcdd823ebe16c2d63711596b2
BenteMariaSon <bente.vanson2@ru.nl>
2026-03-25 15:39:38 +0100

    Removed preprocessing, should be done through chemsy

--- RSSchemometrics/data_processing/PreProcessing.py
+++ RSSchemometrics/data_processing/PreProcessing.py
@@ -1,10 +1,16 @@
+'''
+This preprocessing code works together with Chemsy git repository. The 'Gerretzen' function can be used as an 
+input to the 'solver' argument in the SupervisedChemsy code. The solutions obtained from running SupervisedChemsy
+can be used as an input for the 'pp_doe' function to obtaine the DoE results for visualisation. 
+
+Preprocessing methods themselves should be imported from the chemsy repository. 
+'''
+
 import pandas as pd
 import numpy as np
 from sklearn.base import TransformerMixin, BaseEstimator
 import statsmodels.api as sm
 from itertools import combinations
-from scipy.signal import savgol_filter
-from sklearn.preprocessing import StandardScaler
 
 #===================================================================================================================================================#
 
@@ -31,203 +37,6 @@ class MeanCentering(BaseEstimator,TransformerMixin):
     def fit_transform(self,X,y=None):
       self.fit(X)
       return self.transform(X) 
-
-#===================================================================================================================================================#
-
-class SNV(BaseEstimator,TransformerMixin):
-    def __init__(self):
-      self.__name__='SNV'
-
-    def fit(self,X):
-      pass
-    def transform(self,X, y=None):
-      try:
-        X=pd.DataFrame(X)
-        if X.shape[1] == 1:
-          X=X.T
-      except:
-        pass
-      R=(X.subtract(X.mean(axis=1), axis=0)).divide(X.std(axis=1)+np.finfo(float).eps, axis=0)
-      return R
-    def fit_transform(self,X,y=None):
-      try:
-        X=pd.DataFrame(X)
-        if X.shape[1] == 1:
-          X=X.T
-      except:
-        pass
-      self.fit(X)
-      return self.transform(X)
-    
-#===================================================================================================================================================#
-
-class BaselineOffset(BaseEstimator, TransformerMixin):
-    """Remove baseline offset from the data
-    """
-    def __init__(self):
-        self.fitted_ = False
-    def fit(self, X, y=None):
-        """Not used
-        """
-        self.fitted_ = True
-    def transform(self, X, y=None):
-        """Removes baseline offset from data
-        Args:
-            X (Dataframe): Dataframe of samples [samples x variables] used to train pls_model
-            y (_type_, optional): Not used. Defaults to None.
-        Returns:
-            Dataframe: X with baseline offset removed
-        """
-        X_new = X.sub(X.min(axis=1), axis=0)
-        return X_new
-    def fit_transform(self, X, y=None):
-        """Removes baseline offset from data
-        Args:
-            X (Dataframe): Dataframe of samples [samples x variables] used to train pls_model
-            y (_type_, optional): Not used. Defaults to None.
-        Returns:
-            Dataframe: X with baseline offset removed
-        """
-        return self.transform(X)
-
-#===================================================================================================================================================#
-
-class BaselineLinear(BaseEstimator, TransformerMixin):
-    """Removes linear baseline
-    """
-    def __init__(self):
-        """Not used
-        """
-        self.fitted_ = False
-    def fit(self, X, y=None):
-        """Not used
-        """
-        self.fitted_ = True
-    def transform(self, X, y=None):
-        """Removes linear baseline
-        Args:
-            X (Dataframe): Dataframe of samples [samples x variables] used to train pls_model
-            y (_type_, optional): Not used. Defaults to None.
-        Returns:
-            Dataframe: X with linear baseline removed
-        """
-        X_new=X.copy()
-        grid = np.arange(X.shape[1])
-        for i in range(len(X_new)):
-            y0, y1 = X.iloc[i,0], X.iloc[i,-1]
-            grid0, grid1 = grid[0], grid[-1]
-            # Compute the linear baseline between endpoints
-            baseline = y0 + (grid - grid0) * (y1 - y0) / (grid1 - grid0)
-            X_new.iloc[i,:] = X.iloc[i,:].sub(baseline)            
-        return X_new
-    def fit_transform(self, X, y=None):
-        """Removes linear baseline
-        Args:
-            X (Dataframe): Dataframe of samples [samples x variables] used to train pls_model
-            y (_type_, optional): Not used. Defaults to None.
-        Returns:
-            Dataframe: X with linear baseline removed
-        """
-        return self.transform(X)
-
-#===================================================================================================================================================#
-
-class FirstDerivative(BaseEstimator, TransformerMixin):
-    """Takes first derivate of data
-    """
-    def __init__(self,d=2):
-        """Order to use
-        Args:
-            d (int, optional): derivate order to use. Defaults to 2.
-        """
-        self.d=d  
-        self.fitted_ = False
-    def fit(self, X, y=None):
-        """not used
-        """
-        self.fitted_ = True
-    def transform(self, X, y=None): 
-        """Takes first derivate of data
-        Args:
-            X (Dataframe): Dataframe of samples [samples x variables] used to train pls_model
-            y (_type_, optional): Not used. Defaults to None.
-        Returns:
-            Dataframe: First derivative of X
-        """
-        X_=X.diff(self.d,axis=1)
-        X_.iloc[:, :self.d] = 0 
-        return X_
-    def fit_transform(self,X,y=None):
-        """Takes first derivate of data
-        Args:
-            X (Dataframe): Dataframe of samples [samples x variables] used to train pls_model
-            y (_type_, optional): Not used. Defaults to None.
-        Returns:
-            Dataframe: First derivative of X
-        """
-        return self.transform(X)
-
-#===================================================================================================================================================#
-
-class MSC(BaseEstimator, TransformerMixin):
-    """Performs multiplicative scatter correction
-    """
-    def __init__(self):
-        """not used
-        """
-        self.mean_spectrum_ = None
-        self.fitted_ = False
-    def fit(self, X, y=None):
-        """Takes first derivate of data
-        Args:
-            X (Dataframe): Dataframe of samples [samples x variables] used to train pls_model
-            y (_type_, optional): Not used. Defaults to None.
-        """
-        self.fitted_ = True
-        # Store mean spectrum (as numpy array for performance)
-        try: X_ = pd.DataFrame(X)
-        except: pass
-        self.mean_spectrum_ = X_.mean(axis=0).values
-        return self
-    def transform(self, X, y=None):
-        """Apply MSC
-
-        Args:
-            X (Dataframe): Dataframe of samples [samples x variables] used to train pls_model
-            y (_type_, optional): Not used. Defaults to None.
-
-        Raises:
-            RuntimeError: You must fit the transformer before calling transform.
-
-        Returns:
-            Dataframe: MSC of X
-        """
-        if self.mean_spectrum_ is None:
-            raise RuntimeError("You must fit the transformer before calling transform.")
-        try: X_ = pd.DataFrame(X)
-        except: pass
-        X_corrected = []
-        ref = self.mean_spectrum_
-        for i in range(X_.shape[0]):
-            sample = X_.iloc[i].values
-            # Linear regression: y = a + b * x
-            b, a = np.polyfit(ref, sample, 1)
-            corrected = (sample - a) / b
-            X_corrected.append(corrected)
-
-        # Return as DataFrame with original index and columns
-        return pd.DataFrame(X_corrected, index=X_.index, columns=X_.columns)
-    def fit_transform(self, X, y=None):
-        """First fit, then transform data
-
-        Args:
-            X (Dataframe): Dataframe of samples [samples x variables] used to train pls_model
-            y (_type_, optional): Not used. Defaults to None.
-
-        Returns:
-            Dataframe: MSC of X
-        """
-        return self.fit(X).transform(X)
 
 #===================================================================================================================================================#
 
