07be95e99bf07855150007ce1e48998e54110e24
BenteMariaSon <bente.vanson2@ru.nl>
2026-07-29 11:16:05 +0200

    integration of variable importance functions

--- RSSchemometrics/PLSR.py
+++ RSSchemometrics/PLSR.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from .Plotting.ComponentPlots import *
 from .Plotting.ValidationPlots import *
 from .data_processing.PreProcessing import MeanCentering
+from .variable_importance import VIP, sMC
 from sklearn.model_selection import KFold, cross_validate, cross_val_predict
 from sklearn.base import BaseEstimator
 from sklearn.cross_decomposition import PLSRegression
@@ -161,63 +162,30 @@ class PLSR(BaseEstimator):
         """Calculate and return the Variable Importance Projection (VIP) scores of the fitted PLS model. 
         Implemented as discribed in Mehmood, et al. (2012). A review of variable selection methods in Partial Least Squares Regression 
         
+        Returns:
+            - VIP (ndarray): The Variable Importance Projection values for each feature
         """
         if self.is_fitted_==False:
             raise ValueError("Model has not been fitted yet, call fit() first.")
         
-        t = self.x_scores
-        q = self.y_loadings
-        w = self.x_weights
-        n_features = self.x_loadings.shape[0]
-        
-        s = np.diag(t.T @ t @ q.T @ q) # sum of squares explained by each component (SS_a in article)
-        total_s = np.sum(s)
-        
-        vip = np.zeros(n_features)
-        for i in range(n_features):
-            weight = np.array([(w[i,a]**2)/np.sum(w[:,a]**2) for a in range(self.n_components)])
-            vip[i] = np.sqrt(n_features * np.sum(s * weight) / total_s)
-        return vip
+        return VIP(self.x_scores, self.y_loadings, self.x_weights)
     
     def calculate_sMC(self, X):
         """Calculate and return the Significance Multivariate Correlation (sMC) values of the fitted PLS model. 
         Implemented as discribed in Tran, et al (2014) Interpretation of variable importance in Partial Least Squares with Significance Multivariate Correlation (sMC).
         
         Args:
-            - X (ndarray): The data matrix that was used for fitting
+            - X (ndarray of shape (n_samples, n_features)): The data matrix that was used for fitting the PLS model from which B was obtained
+            
+        Returns:
+            - sMC (ndarray): Significance Multivariate Correlation f-values for each feature
+            - sMC_p (ndarray): sMC transformed to p-values
         """
         if self.is_fitted_==False:
             raise ValueError("Model has not been fitted yet, call fit() first.")
         
-        if self.scale:
-            X = np.asarray(StandardScaler().fit_transform(X))
-        else:
-            X = np.asarray(MeanCentering().fit_transform(X))
-        n_samples, n_features = X.shape
+        return sMC(self.coef, X, scale=self.scale)
         
-        B = np.asarray(self.coef) # shape (n_target, n_features)
-        # if B is only one dimension, we convert it to two dimensional
-        if B.ndim == 1:
-            B = B.reshape(1, n_features) 
-        n_targets = B.shape[0]
-        
-        smc_values = np.zeros((n_features, n_targets))
-        p_values = np.zeros((n_features, n_targets))
-        for target in range(n_targets): # if PLS2 we run this loop multiple times, for PLS1 just once
-            b = B[target, :]
-            
-            y_hat = X @ b # predicted y vector (eq 15)
-            X_hat = np.outer(y_hat, b) / np.linalg.norm(b)**2 # predicted X (eq 16)
-            resid = X - X_hat # (eq 16)
-            
-            SS_model = np.sum(X_hat**2) # eq 18
-            SS_resid = np.sum(resid**2) # eq 19
-            
-            smc_values[:, target] = (SS_model / (SS_resid/(n_samples-2))) # eq 22
-            p_values[:, target] = (1 - f.cdf(smc_values[:,target], 1, n_samples-2)) # f-test conversion to p-values
-                     
-        return np.squeeze(smc_values), np.squeeze(p_values) # use squeeze to remove added dimensions
-
     def plot_scree(self, n_components=None, color=rucolors.red):
         """Plots a scree plot to visualize the explained variance of each latent variable
         Args:
